576f3aedb90d90c09b429f329ca98c3768ec2f17
Nikita Khveshchenko <nikita.khveshchenko@yandex.ru>
2026-03-05 19:01:55 +0300

    feat: add html markup, section and lock swiper js section hero

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,21 @@
+import "swiper/css";
 import "../styles/index.css";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
-console.log("Hello world!");
+const heroSlider = new Swiper(".slider", {
+  modules: [Pagination, Navigation],
+  spaceBetween: 20, // расстояние между слайдами
+  slidesPerView: 1, // сколько отображается слайдов
+  direction: "horizontal",
+  speed: 800,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next"
+  }
+});
